f4383c9e8f4b4abdc0641968ffaaca2113b4dd54
manuelcanorueda <manuel.cano.rueda@alumnos.upm.es>
2018-05-05 19:47:21 +0200

    P5_Quiz

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const models = require("../models/index.js");
+const {models} = require("../models/index.js");
 
 var express = require("express");
 var router = express.Router();
@@ -8,11 +8,11 @@ router.get('/', (req, res, next) => {
 	res.render('index', {title: "P5_QUIZ"});
 });
 
-router.get('/credits', function(req, res, next) {
+router.get('/credits', (req, res, next) => {
 	res.render('credits', {title: "P5_QUIZ", name: "Manuel Cano Rueda"});
 });
 
-router.get('/quizzes', function(requ, res, next) {
+router.get('/quizzes', (requ, res, next) => {
 	models.quiz.findAll().then( (quizzes) => {
 		res.render('quizzes', {quizzes});
 	})
